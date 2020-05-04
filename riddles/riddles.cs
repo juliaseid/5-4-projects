1cf0275eae306f156697eaf85e11a3cd8d116553
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-04 11:23:07 -0700

    add while loop to randomized questions

--- riddles/riddles.cs
+++ riddles/riddles.cs
@@ -6,19 +6,29 @@ class Sphinx
 
   static void Main()
   {
-    string[] riddles = { "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?", "What kind of a goose eats snakes?", "What 4-letter word can be written forward, backward or upside down, and can still be read from left to right?" };
-    string[] answers = { "Fire", "A mongoose", "NOON" };
-    var random = new Random();
-    int riddleIndex = random.Next(0, 2);
-    Console.WriteLine(riddleIndex);
-    string riddle1 = riddles[riddleIndex];
-    Console.WriteLine(riddle1);
-    string riddle1Ans = Console.ReadLine();
-    if (riddle1Ans == answers[riddleIndex])
+    bool alive = true;
+
+    while (alive == true)
     {
-      Console.WriteLine("That is correct! You live to die another day!");
+
+      string[] riddles = { "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?", "What kind of a goose eats snakes?", "What 4-letter word can be written forward, backward or upside down, and can still be read from left to right?" };
+      string[] answers = { "Fire", "A mongoose", "NOON" };
+      var random = new Random();
+      int riddleIndex = random.Next(0, 2);
+      Console.WriteLine(riddleIndex);
+      string riddle1 = riddles[riddleIndex];
+      Console.WriteLine(riddle1);
+      string riddle1Ans = Console.ReadLine();
+      if (riddle1Ans == answers[riddleIndex])
+      {
+        Console.WriteLine("That is correct! You live to die another day!");
+      }
+      else
+      {
+        Console.WriteLine("It looks like today is your day to die. The Sphinx is going to eat you.");
+        alive = false;
+      }
     }
+
   }
-
-
 }
