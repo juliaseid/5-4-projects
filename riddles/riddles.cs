3d04d9b2d7eca87b867a310618e796e0eee33eaf
Ethan Firpo <suzannedeakins@Ethan-Firpo-CyberDeck.local>
2020-05-04 11:04:37 -0700

    add randomized riddles program

---
+++ riddles/riddles.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class Sphinx
+{
+
+  static void Main()
+  {
+    string[] riddles = { "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?", "What kind of a goose eats snakes?", "What 4-letter word can be written forward, backward or upside down, and can still be read from left to right?" };
+    string[] answers = { "Fire", "A mongoose", "NOON" };
+    var random = new Random();
+    int riddleIndex = random.Next(0, 2);
+    Console.WriteLine(riddleIndex);
+    string riddle1 = riddles[riddleIndex];
+    Console.WriteLine(riddle1);
+    string riddle1Ans = Console.ReadLine();
+    if (riddle1Ans == answers[riddleIndex])
+    {
+      Console.WriteLine("That is correct! You live to die another day!");
+    }
+  }
+
+
+}
